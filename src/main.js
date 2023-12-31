41c59b357fa54863e22d132bc503e4d416c099d7
Maezzzkurel <escurelmae@gmail.com>
2023-12-31 12:30:57 +0800

    Escurel Mae Project: Microtool

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+// import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/index.umd.js'
 
 createApp(App).mount('#app')
+
